7db12ae91c59fec8a28246655277e3c882813f9f
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-05-04 21:46:56 +0100

    game() position altered

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+console.log(game());
+
+function game() {
 
 // random selection using math.floor(math.random() * 3)) and reassigning values to r/p/s //
 function computerPlay() {
-    const number = (Math.floor(Math.random() * 3));
+    let number = (Math.floor(Math.random() * 3));
     if (number === 0) {
         return "rock";
     } else if (number === 1) {
@@ -13,8 +16,7 @@ let computerSelection = computerPlay();
 let userSelection;
 let computerScore;
 let userScore;
-
-function game() {
+{
     computerScore = 0;
     userScore = 0;
     for (let i=0; i<5; i++) {
@@ -37,18 +39,17 @@ function userInput() {
 
 // else / if conditionals for result declerations //
 function playRound(computerSelection, userSelection) {
-    if (computerSelection === userSelection) {
+    if (computerSelection == userSelection) {
         return `Oooh, you've cancelled eachother out! It's a tie! PLAYER ${userScore} vs. CPU ${computerScore}`;
     }
-    else if (computerSelection === "rock" && userSelection === "scissors" || 
-    computerSelection === "scissors" && userSelection === "paper" || 
-    computerSelection === "paper" && userSelection === "rock") {
+    else if (computerSelection == "rock" && userSelection == "scissors" || 
+    computerSelection == "scissors" && userSelection == "paper" || 
+    computerSelection == "paper" && userSelection == "rock") {
         ++computerScore;
         return `Oh dear! Computer wins the round! PLAYER ${userScore} vs. CPU ${computerScore}`;
     } else ++userScore;
     return `Yes! You Win the round! PLAYER ${userScore} vs. CPU ${computerScore}`;
         
 } 
-}
+}} 
 
-console.log(game());
