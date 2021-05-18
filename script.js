8e19cffb2bc612aa3000acaf0bfec9122886c2cb
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-05-19 00:06:12 +0100

    added scoreboard

--- script.js
+++ script.js
@@ -3,13 +3,25 @@
 let userScore = 0;
 let computerScore = 0;
 
-// SCORE BOARD AND ROUND RESULT //
+// SCORE BOARD //
 
-const latestResults = document.querySelector('.results');
+function getScore() {
+    score.textContent = `PLAYER ${userScore} vs. CPU ${computerScore}`;
+    }
+
+const resultContainer = document.querySelector('.results');
 const results = document.createElement('div');
 results.classList.add('results');
-latestResults.appendChild(results);
-results.textContent = "Pick your first weapon of choice!";
+resultContainer.appendChild(results);
+results.textContent = "First to 5 wins! Pick your first weapon of choice!";
+
+const scoreContainer = document.querySelector('.scoreboard');
+const score = document.createElement('div');
+score.classList.add('score');
+scoreContainer.appendChild(score);
+
+
+
 
 // WINNER DECLARATION //
 
@@ -26,6 +38,7 @@ function reset(){
     userScore = 0;
     computerScore= 0;
     results.textContent = "What are you waiting for? Play again!";
+    getScore();
 }}
 
 
@@ -46,31 +59,38 @@ function computerPlay() {
     function playRound(computerSelection, userSelection) {
         computerPlay();
     if (computerSelection === userSelection) {
-        return `Oooh, you've cancelled eachother out! It's a tie! PLAYER ${userScore} vs. CPU ${computerScore}`;
+        getScore();
+        return `Oooh, you've cancelled eachother out! It's a tie!`;
     }
     else if (computerSelection === "rock" && userSelection === "scissors") {
         ++computerScore;
-        return `Dagnabbit! The computer selected "Rock" and broke your Scissors! PLAYER ${userScore} vs. CPU ${computerScore}`;
+        getScore();
+        return `Dagnabbit! The computer selected "Rock" and broke your Scissors!`;
     }  
     else if (computerSelection === "scissors" && userSelection === "paper") {
         ++computerScore;
-        return `Oh no, The computer selected "Scissors" and cut right through your paper! Bummer.. PLAYER ${userScore} vs. CPU ${computerScore}`;
+        getScore();
+        return `Oh no, The computer selected "Scissors" and cut right through your paper! Bummer...`;
     } 
     else if (computerSelection === "paper" && userSelection === "rock") {
         ++computerScore;
-        return `Deary me! The computer selected "Paper" and wrapped your rock up like a birthday gift! PLAYER ${userScore} vs. CPU ${computerScore}`;
+        getScore();
+        return `Deary me! The computer selected "Paper" and wrapped your rock up like a birthday gift!`;
     }   
     else if (computerSelection === "scissors" && userSelection === "rock") {
         ++userScore;
-        return `Yeah! The computer selected "Scissors" - you broke them with your Rock! PLAYER ${userScore} vs. CPU ${computerScore}`;
+        getScore();
+        return `Yeah! The computer selected "Scissors" - you broke them with your Rock!`;
     }  
     else if (computerSelection === "paper" && userSelection === "scissors") {
         ++userScore;
-        return `Alright! The computer selected "Paper" - you cut it to pieces with your Scissors! Yes! PLAYER ${userScore} vs. CPU ${computerScore}`;
+        getScore();
+        return `Alright! The computer selected "Paper" - you cut it to pieces with your Scissors! Yes!`;
     }
     else if (computerSelection === "rock" && userSelection === "paper") {
         ++userScore;
-        return `You love to see it! The computer selected "Rock" and you wrapped it up with your Paper! Victory! PLAYER ${userScore} vs. CPU ${computerScore}`;
+        getScore();
+        return `You love to see it! The computer selected "Rock" and you wrapped it up with your Paper! Victory!`;
     }
 }
 
@@ -80,7 +100,7 @@ const selectionButton = document.querySelectorAll('img');
 selectionButton.forEach(img => img.addEventListener('click', playGame));
 
 function playGame(e){
-    let userSelection = (e.target.id); 
+    let userSelection = (e.target.id);
     const computerSelection = computerPlay(); 
     results.textContent = playRound(computerSelection, userSelection);
     console.log(userSelection);
